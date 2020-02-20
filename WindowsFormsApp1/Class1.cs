f24cfc9c8833f0d0ea8663553fc79c0d3b9a0b56
nisiyuu <west12capri12@gmail.com>
2020-02-20 23:52:46 +0900

    About Class,Instance,constructor,signeture,overload

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        ///
+        public Class1(){}
+        public Class1(int num){　//自作のインストラクタ　Class1型のインスタンス
+            this.test = num;
+        }
+        /// <summary>
+        /// 文字列を引数にとるコンストラクタ
+        /// </summary>
+        /// <param name="str">あいさつ</param>
+        public Class1(string str)
+        {
+            this.str = str;
+        }
+        private string str = "おはよう";//組み込み　int型のインスタンス変数
+        private int test = 1;//組み込み　int型のインスタンス変数
+        public const int test2 = 2;
+        public int showTest()　//組み込み　int型のインスタンスメソッド
+        {
+            return this.test;  //privateだと関数をけいゆしてでないと参照できない
+        }
+        public int showTest(int num)
+        {
+            return this.showTest() + num;
+        }
+
+    }
+}
+
+
+//publicとprivate publicであればどこからでも参照できる＆privateだと同じクラス内でしか参照できない
+//Form1.csからprivateのtestを呼び出すには、例えばpublicのshowTestで囲んであげることで可能
+
+//同じコンストラクタ名でいくつか種類がつくれる シグネチャが異なる
+//引数なしのインスタンスと・・・A
+//int型引数をもつインスタンスなど・・・B
+
+//BはAの機能+int型引数を活用した機能
+//これがオーバーライド
+
+//参照型のほとんどはobject型これを継承してstring型になったりする
+//https://docs.microsoft.com/ja-jp/dotnet/api/system.string?view=netframework-4.8#methods
+//https://docs.microsoft.com/ja-jp/dotnet/api/system.object?view=netframework-4.8
+//くらすが継承されていることがわかる
+//例えばStringクラスGetTypeは(継承元 Object)と書かれている。
+
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var instance1 = new Class1();
+            var instance4 = new Class1().showTest();
+            var instance2 = new Class1(100);
+            var instance3 = new Class1();
+            Object obj1 = new Class1();//アップキャスト　Object型がツリー構造の上位 <実務ではあまりみないが、interfaceへの考え方の布石>
+            //obj1.//showTestというインスタンスメソッドはないなぜならObjectにはその定義がないから
+            Console.WriteLine(instance1.showTest());
+        }
+
+    }
+}
